2c04963798d681ef8f7521bcf546d3df48b844c3
Mohammad Kaif <mohammad766kaif@gmail.com>
2024-02-29 21:34:49 +0530

    first commit

---
+++ kaif_react_app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Resturant from "./components/Basics/Resturant"
+
+const App = () => {
+    return ( <
+        div >
+        <
+        Resturant / >
+        <
+        /div>
+    )
+}
+
+export default App
